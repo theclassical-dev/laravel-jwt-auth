2399b7fe6880eb2fe06dc548d710f57975c6e177
Abraham Peter <theclassical001@gmail.com>
2022-03-05 22:11:08 +0100

    update + signoutcontroller created

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>'v1', 'namespace'=>'Auth'], function () {
     Route::post('/signin', 'SignInController');
     Route::get('/user', 'MainController');
-    Route::post('/logout', 'MainController@logout');
+    Route::post('/logout', 'SignOutController');
 });
